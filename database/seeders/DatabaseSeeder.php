d90a4825a4da1a780434702af787ed1be5d8b90d
Mishkat Hossain <hossainmishkat0@gmail.com>
2024-05-13 05:10:29 +0600

    working

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +14,58 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(5)->create();
+        
+        Listing::factory(6)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+
+
+
+
+        // Listing::create([
+        //     'title' => 'Laravel Senior Developer',
+        //     'tags' => 'laravel, javascript',
+        //     'company' => 'Acme Cork',
+        //     'location' => 'Boston, MA',
+        //     'email' => 'acme@gmail.com',
+        //     'website' => 'www.acme.com',
+        //     'description' => '
+        //     Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+        //     molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+        //     numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+        //     optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+        //     obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+        //     nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+        //     tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+        //     quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
+        //     sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
+        //     recusandae alias error harum maxime adipisci 
+        //     ',
+        // ]);
+
+
+        // Listing::create([
+        //     'title' => 'Laravel L2 Developer',
+        //     'tags' => 'laravel, javascript',
+        //     'company' => 'max Corp',
+        //     'location' => 'Miami, MA',
+        //     'email' => 'max@yahoo.com',
+        //     'website' => 'www.max.com',
+        //     'description' =>'amet laborum. Perspiciatis 
+        //     minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit 
+        //     quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur 
+        //     fugiat, temporibus enim commodi iusto libero magni deleniti quod quam 
+        //     consequuntur! Commodi minima excepturi repudiandae velit hic maxime
+        //     doloremque. Quaerat provident commodi consectetur veniam similique ad 
+        //     earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo 
+        //     fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore 
+        //     suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
+        //     modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam 
+        //     totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam 
+        //     quasi aliquam eligendi, placeat qui corporis!'
+        // ]);
+
+
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
+use App\Models\Listing;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ListingController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Common Resource Routes:
+// index
+// show
+// create
+// store
+// edit 
+// update 
+// destroy
+
+
+// All listing
+Route::get('/', [ListingController::class, 'index']);
+
+// Show Create form
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+//Store Listing data
+Route::post('/listings',  [ListingController::class, 'store']); 
+
+// Show Edit form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+// Single listing
+Route::get('/listings/{listing}',  [ListingController::class, 'show']); 
+
